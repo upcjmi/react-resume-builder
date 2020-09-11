313dd3a9691727a6518bec59a95296373c122f22
root <alhaqhassan@gmail.com>
2020-09-11 20:04:03 +0530

    Updates

---
+++ precache-manifest.12f7e845bdfde8d7c32f520ef0391d63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25d773d8ecf5954932969ccc63568df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6052e8e655ecadbb9860",
+    "url": "/static/css/2.8d4cdce2.chunk.css"
+  },
+  {
+    "revision": "a0fae879e929eeff96e6",
+    "url": "/static/css/main.b6364d22.chunk.css"
+  },
+  {
+    "revision": "6052e8e655ecadbb9860",
+    "url": "/static/js/2.972497cb.chunk.js"
+  },
+  {
+    "revision": "c5f0225e56fd389382d353240173294c",
+    "url": "/static/js/2.972497cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0fae879e929eeff96e6",
+    "url": "/static/js/main.7a14f855.chunk.js"
+  },
+  {
+    "revision": "c326467593c7c9e26619",
+    "url": "/static/js/runtime-main.c525a809.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
